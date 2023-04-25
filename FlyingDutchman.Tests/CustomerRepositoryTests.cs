8777ba01392aedddf6159819fa9b41dbdc83f551
Alexandr Iurashchuk <a.iurashchuk@gmail.com>
2023-04-25 13:50:37 +0200

    CustomerRep's little functionality

--- FlyingDutchman.Tests/CustomerRepositoryTests.cs
+++ FlyingDutchman.Tests/CustomerRepositoryTests.cs
@@ -10,5 +10,66 @@ public class CustomerRepositoryTests
     {
         var repository = new CustomerRepository();
         Assert.IsNotNull(repository);
+
+        var result = repository.CreateCustomer("Fernando Alonso");
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void CreateCustomerFailureNameIsNull()
+    {
+        var repository = new CustomerRepository();
+        Assert.IsNotNull(repository);
+
+        var result = repository.CreateCustomer(null);
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void CreateCustomerFailureNameIsEmpty()
+    {
+        var repository = new CustomerRepository();
+        Assert.IsNotNull(repository);
+
+        var result = repository.CreateCustomer(string.Empty);
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    [DataRow('!')]
+    [DataRow('@')]
+    [DataRow('#')]
+    [DataRow('$')]
+    [DataRow('%')]
+    [DataRow('^')]
+    [DataRow('&')]
+    [DataRow('*')]
+    [DataRow('(')]
+    [DataRow(')')]
+    [DataRow(',')]
+    [DataRow('.')]
+    [DataRow('?')]
+    [DataRow('"')]
+    [DataRow(':')]
+    [DataRow('{')]
+    [DataRow('}')]
+    [DataRow('|')]
+    [DataRow('<')]
+    [DataRow('>')]
+    [DataRow('[')]
+    [DataRow(']')]
+    [DataRow('_')]
+    [DataRow('-')]
+    [DataRow('+')]
+    [DataRow('=')]
+    [DataRow('/')]
+    [DataRow('\\')]
+    public void CreateCustomerFailureNameContainsInvalidChars(char invalidChar)
+    {
+        var repository = new CustomerRepository();
+        Assert.IsNotNull(repository);
+
+        var result = repository.CreateCustomer($"Biba{invalidChar}");
+        Assert.IsFalse(result);
     }
 }
--- FlyingDutchmanAirlines/RepositoryLayer/CustomerRepository.cs
+++ FlyingDutchmanAirlines/RepositoryLayer/CustomerRepository.cs
@@ -2,5 +2,14 @@ namespace FlyingDutchmanAirlines.RepositoryLayer;
 
 public class CustomerRepository
 {
-    public void CreateCustomer(string name) { }
+    public bool CreateCustomer(string name)
+    {
+        return !NameIsInvalid(name);
+    }
+
+    private bool NameIsInvalid(string name)
+    {
+        var specialCharacters = new char[] { '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', ',', '.', '?', '"', ':', '{', '}', '|', '<', '>', '=', '+', '/', '\\', '-', '_', '[', ']' };
+        return string.IsNullOrEmpty(name) || name.Any(x => specialCharacters.Contains(x));
+    }
 }
